f58148fe3e2b18f952ac4e01f0e3790dc6526931
Laura Trager <laura.trager@gmail.com>
2017-12-12 18:29:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
 
-  attr_accessor :side_a, :side_b, :side_c, :type
+  attr_reader :side_a, :side_b, :side_c
 
   def initialize(side_a, side_b, side_c)
     @side_a = side_a
@@ -10,17 +10,17 @@ class Triangle
 
   def kind
     validate_triangle
-    if @side_a == @side_b && @side_a == @side_c #all sides
-      @type = :equilateral
-    elsif @side_a == @side_b || @side_c == @side_b || @side_a == @side_c #two sides
-      @type = :isosceles
-    else #@side_a != @side_b && @side_a != @side_c #no sides
-      @type = :scalene
+    if side_a == side_b && side_a == side_c #all sides
+      :equilateral
+    elsif side_a == side_b || side_c == side_b || side_a == side_c #two sides
+      :isosceles
+    else #no sides
+      :scalene
     end
   end
 
   def validate_triangle
-    if @side_a <= 0 || @side_b <= 0 || @side_c <= 0 || @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_c + @side_b <= @side_a
+    if side_a <= 0 || side_b <= 0 || side_c <= 0 || side_a + side_b <= side_c || side_a + side_c <= side_b || side_c + side_b <= side_a
       raise TriangleError
     end
   end
